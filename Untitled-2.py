cf430e8a1bd7673d2722ea9375da7710fcfeb452
Md. Amzad Hossain Notun <110979018+AmzadHossainNotun@users.noreply.github.com>
2022-10-09 22:30:22 +0600

    Add files via upload

---
+++ Untitled-2.py
@@ -0,0 +1,7 @@
+age = int(input("enter age: "))
+
+if age >=18:
+                    print("eligible for voting")
+else:
+                    print("not eligible for voting")
+                    
---
+++ till 0 using recursion.py
@@ -0,0 +1,7 @@
+def print_till_zero(n):
+    if (n==0):
+        return
+    print(n)
+    n=n-1
+    print_till_zero(n)
+print_till_zero(8)
